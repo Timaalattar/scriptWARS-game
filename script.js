f2fcd1c15a2c39a2efab0d728bd9e0a5ccc18bf2
Timaalattar <fatima.alattar@gmail.com>
2022-10-03 17:04:39 +0300

    defining the functions for answering correctly or wrong

--- script.js
+++ script.js
@@ -49,9 +49,32 @@ function myFunction_start() {
     document.getElementById("button-start").style.display = 'none'; 
     document.getElementById("game-start").style.display = 'flex'; 
     document.getElementById("game-start").style.justifyContent = 'center';
-    document.getElementById("instruction-text").innerHTML = 'Player 1 turn';
-    console.log(questionsAndAnswers[randomQuestion]) 
-    console.log(questionsAndAnswers) 
-    console.log(randomQuestion) 
+    document.getElementById("instruction-text").innerHTML = 'Player 1 turn'; 
     document.getElementById("game-text").innerHTML = questionsAndAnswers[randomQuestion].question;
+}
+
+// next I need to code the game function after the loading/starting the game
+// - after questions appears it is player 1's turn to start they have to be able to type the answer in the input box and press submit
+// - if the question is right then player's score is to be updated with 1 point
+// - if the question is wrong then the turn needs to move to player 2 
+// - if player 2 answers the question correctly then their score has to be updated and then they go again for their turn 
+// if the answer is wrong then we have to show the question and have a button to click to for the next question, but it will still be player 2 turn
+
+let userAnswer = document.querySelector('answers').addEventListener('input', answerQuestion)
+
+function answerQuestion(event) {
+  let answer = questionsAndAnswers[randomQuestion].answer
+  let playerOneScore = 0 
+  let playerTwoScore = 0
+  if (userAnswer == answer) {
+    function myFunction_player() {
+        document.getElementById("player1-score").innerHTML = playerOneScore + 1; 
+        document.getElementById("new-question").style.display = 'flex'; 
+        document.getElementById("new-question").style.justifyContent = 'center';
+  } else {
+    if (userAnswer !== answer) {
+        function myFunction_nextplayer() {
+            document.getElementById("player1-score").innerHTML = 'Player 1 turn'; 
+  }
+  }
 }
