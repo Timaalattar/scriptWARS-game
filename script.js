08e416ee955c8465a1099ed2b4e4a0e9d0b583b9
Timaalattar <fatima.alattar@gmail.com>
2022-10-03 11:57:15 +0300

    finished basic CSS start javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+//creating the fuctions that start the game.
+// - players click on start game 
+// - instruction text is to disappear 
+// - "player 1 plays text to load"
+// - first question to appear 
+// - input box for text is to appear 
+// - submit method to appear 
+
+//click game start button 
+
+function myFunction_start() {
+onclick = document.getElementById("button-start").style.display = 'none'
+}
